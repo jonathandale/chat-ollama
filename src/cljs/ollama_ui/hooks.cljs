ab9dc22099b89fa4779cd7114a86992ab09ce98d
Jon Dale <hi@j-d.co>
2023-09-14 21:36:15 -0400

    Add clipboard copy hook

---
+++ src/cljs/ollama_ui/hooks.cljs
@@ -0,0 +1,19 @@
+(ns ollama-ui.hooks
+  (:require [applied-science.js-interop :as j]
+            [helix.hooks :refer [use-effect use-state]]))
+
+
+(defn use-copy-to-clipboard []
+  (let [[copied set-copied!] (use-state false)
+        clipboard (j/get js/navigator :clipboard)
+        copy! #(do
+                 (j/call clipboard :writeText %)
+                 (set-copied! true))]
+    (use-effect
+     [copied]
+     (when copied
+       (let [wait (js/setTimeout #(set-copied! false) 1500)]
+         #(js/clearTimeout wait))))
+
+    (when clipboard
+      [copied copy!])))
